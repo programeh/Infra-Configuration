1f84abef77583e9c3d221d92f55f1a1dce5bdfe8
Asutosh Ghanto <asutosh@clevertap.com>
2023-08-25 23:47:46 +0530

    terraform: pushing code for infra config in laksh account.

---
+++ staging/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-20230825133837764000000001"
+    key    = "staging/terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers{
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
