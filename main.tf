08a598044d16ff4ded949eef223860e876cb59dd
Asutosh Ghanto <asutosh@clevertap.com>
2023-11-14 00:17:46 +0530

    git: push region wise resources.

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 module "vpc" {
+  for_each = var.data_centers
   source                       = "./modules/vpc"
-  region                       = "ap-south-1"
-  project_name                 = "dummy_project"
-  vpc_cidr                     = "10.0.0.0/16"
-  public_subnet_az1_cidr       = "10.0.0.0/24"
-  public_subnet_az2_cidr       = "10.0.1.0/24"
-  private_subnet_az1_cidr      = "10.0.2.0/24"
-  private_subnet_az2_cidr      = "10.0.3.0/24"
-  private_data_subnet_az1_cidr = "10.0.4.0/24"
-  private_data_subnet_az2_cidr = "10.0.5.0/24"
+  region                       = var.region_map["${each.value}"]
+  project_name                 = "${each.value}-dummy_project"
+  vpc_cidr                     = "${var.region_first_two_octet_map["${each.value}"]}.0.0/16"
+  public_subnet_az1_cidr       = "${var.region_first_two_octet_map["${each.value}"]}.0.0/24"
+  public_subnet_az2_cidr       = "${var.region_first_two_octet_map["${each.value}"]}.1.0/24"
+  private_subnet_az1_cidr      = "${var.region_first_two_octet_map["${each.value}"]}.2.0/24"
+  private_subnet_az2_cidr      = "${var.region_first_two_octet_map["${each.value}"]}.3.0/24"
+  private_data_subnet_az1_cidr = "${var.region_first_two_octet_map["${each.value}"]}.4.0/24"
+  private_data_subnet_az2_cidr = "${var.region_first_two_octet_map["${each.value}"]}.5.0/24"
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,26 @@
+variable "region_map" {
+  type = map(any)
+  default = {
+    sk1 = "ap-northeast-2"
+    eu1 = "eu-west-1"
+    in1 = "ap-south-1"
+    sg1 = "ap-southeast-1"
+    us1 = "us-west-2"
+  }
+}
 
+variable "data_centers" {
+  type    = set(string)
+  default = ["sk1","eu1", "in1", "sg1", "us1"]
+}
+
+variable "region_first_two_octet_map" {
+  type = map(any)
+  default = {
+    sk1 = "10.14"
+    eu1 = "10.11"
+    in1 = "10.12"
+    sg1 = "10.15"
+    us1 = "10.16"
+  }
+}
